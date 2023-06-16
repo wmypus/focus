7ea1a360cbdab73a4305db5bfba6579bb41d5d32
wmypus <134991898+wmypus@users.noreply.github.com>
2023-06-15 20:49:57 -0700

    Add files via upload

---
+++ focus.py
@@ -0,0 +1,13 @@
+import time
+
+def countdown(t):
+    while t:
+        mins, secs = divmod(t, 60)
+        timer = '{:02d}:{:02d}'.format(mins, secs)
+        print(timer, end="\r")
+        time.sleep(1)
+        t -= 1
+    print('专注时间结束！')
+
+# 设定专注时间为 25 分钟
+countdown(25*60)
